6b3f85b91fc9d39361f885252130f26730c7b8a8
kushalvenkat <kushalvsk@gmail.com>
2024-09-16 15:22:09 -0500

    Initial commit of calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# calculator.py
+def add(x, y):
+    """Addition"""
+    return x + y
+
+def subtract(x, y):
+    """Subtraction"""
+    return x - y
+
+def multiply(x, y):
+    """Multiplication"""
+    return x * y
+
+def divide(x, y):
+    """Division"""
+    if y == 0:
+        raise ValueError("Cannot divide by zero!")
+    return x / y
